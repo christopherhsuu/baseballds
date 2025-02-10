807e793e6e3eee671387dbb4ca9f793124841ad1
christopherhsuu <christopher.m.hsu@gmail.com>
2025-02-10 15:09:49 +0800

    new

---
+++ vids/velocity.py
@@ -0,0 +1,11 @@
+pitch_velocities = [
+    ['pitch0', 82],
+    ['pitch1', 81],
+    ['pitch2', 80],
+    ['pitch3', 79],
+    ['pitch4', 78],
+    ['pitch5', 77],
+    ['pitch6', 76],
+    ['pitch7', 75],
+    ['pitch8', 74],
+]
